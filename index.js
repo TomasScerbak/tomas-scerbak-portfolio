9f5b8e638a122f10cb580efacacaadfeb719d68d
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-02-07 14:48:58 +0100

    creating grid about me

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const navigationBar = document.querySelector('.navigation--bar');
 const circle = document.querySelector('.fa-times-circle');
 
 hamburger.addEventListener('click', () => {
+    navigationBar.classList.toggle('hidden-left');
     hamburger.classList.add('is-hidden');
     circle.classList.remove('is-hidden');
-    navigationBar.classList.toggle('hidden-left');
 });
 
 circle.addEventListener('click', () => {
