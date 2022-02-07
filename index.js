2266fc1ad3ee96ed743d99416dbf27696e522210
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-02-07 18:52:11 +0100

    moving elements
    moving about me element on top of the section when menu is closing

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
 const hamburger = document.querySelector('.hamburger');
 const navigationBar = document.querySelector('.navigation--bar');
 const circle = document.querySelector('.fa-times-circle');
+const aboutMe = document.querySelector('.about-me');
 
 hamburger.addEventListener('click', () => {
     navigationBar.classList.toggle('hidden-left');
+    aboutMe.style.top = '50%'
     hamburger.classList.add('is-hidden');
     circle.classList.remove('is-hidden');
 });
 
 circle.addEventListener('click', () => {
     hamburger.classList.remove('is-hidden');
+    aboutMe.style.top = '10%'
     circle.classList.add('is-hidden');
     navigationBar.classList.toggle('hidden-left');
 });
