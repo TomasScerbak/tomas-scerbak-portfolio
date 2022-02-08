78e42769d9c6c527d674aa2fe925a451056b2576
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-02-08 10:50:15 +0100

    adding js to hide git on nav toggle

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const hamburger = document.querySelector('.hamburger');
 const navigationBar = document.querySelector('.navigation--bar');
 const circle = document.querySelector('.fa-times-circle');
 const aboutMe = document.querySelector('.about-me');
+const git = document.querySelector('.git');
 
 hamburger.addEventListener('click', () => {
     navigationBar.classList.toggle('hidden-left');
     aboutMe.style.top = '45%'
     hamburger.classList.add('is-hidden');
     circle.classList.remove('is-hidden');
+    git.classList.add('hidden-left');
 });
 
 circle.addEventListener('click', () => {
@@ -16,4 +18,5 @@ circle.addEventListener('click', () => {
     aboutMe.style.top = '13%'
     circle.classList.add('is-hidden');
     navigationBar.classList.toggle('hidden-left');
+    git.classList.remove('hidden-left');
 });
